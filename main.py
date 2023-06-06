fe982dd786e9acc26f21b947803af01ddbcdc67a
Sachin Kant <sachinkant404@gmail.com>
2023-06-06 17:16:58 +0530

    added url input form

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+# from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
