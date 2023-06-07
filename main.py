a36b4e3ea8f0128f60bee6d20d0b9d027439573a
Sachin Kant <sachinkant404@gmail.com>
2023-06-08 02:35:44 +0530

    added url display card

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 # from fastapi.staticfiles import StaticFiles
@@ -11,3 +11,10 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/url_input", response_class= HTMLResponse)
+async def url_input(request: Request, url_input: str = Form(default=None)):
+    if url_input is not None and url_input.strip() != "":
+        print(url_input)
+    return templates.TemplateResponse("index.html", {"request": request, "url_input": url_input})
+    
