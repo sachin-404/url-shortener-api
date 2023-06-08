e403051724f7e56255113f3f9498b91be04dfb2a
Sachin Kant <sachinkant404@gmail.com>
2023-06-08 15:39:57 +0530

    added database orm schema

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request, Form, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 # from fastapi.staticfiles import StaticFiles
+import validators
+from src import schemas
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 # app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+def raise_exception(error_message):
+    raise HTTPException(status_code=400, detail=error_message)
 
-@app.post("/url_input", response_class= HTMLResponse)
-async def url_input(request: Request, url_input: str = Form(default=None)):
-    if url_input is not None and url_input.strip() != "":
-        print(url_input)
-    return templates.TemplateResponse("index.html", {"request": request, "url_input": url_input})
-    
+@app.get("/")
+async def index(request: Request):
+    return "helloooooo"
+    # return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/input_url")
+async def url_input(url: schemas.BaseModel):
+    if not validators.url(url.target_url):
+        return raise_exception("Entered URL is not valid")
+    return url
